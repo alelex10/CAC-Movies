f1192acb16ba3b222933e496cac8080354f40f8c
alelex10 <70962701+alelex10@users.noreply.github.com>
2024-07-09 11:44:23 -0300

    seccion trailes eliomine info duplicada

--- src/main/webapp/js/pageMovie.js
+++ src/main/webapp/js/pageMovie.js
@@ -1,23 +1,27 @@
 
 const API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMDIxMTk5OGEwMWIxMGM1MDk5OGE1YTBkODI3NzI0MSIsInN1YiI6IjY2NTczNjFjODZjYzJiNzJkZjFjZjI5MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3SxeWj4H5Sfe_B2MkiY2fXzXNeFH02UDlH3XVo5N-qI';
 const API_URL = 'https://api.themoviedb.org/3';
-const idioma = "es-MX";
+const idioma = {
+    espanol : "es-MX",
+    ingles : "en-US",
+};
 
 function llamarApi(id) {
-    fetch(`${API_URL}/movie/${id}?language=${idioma}`, {
+    fetch(`${API_URL}/movie/${id}?language=${idioma.espanol}`, {
         headers: {
             Authorization: `Bearer ${API_KEY}`,
         },
     })
-    .then(response => response.json())
-    .then(data => dibujarBanner(data))
-    .catch(error => console.error('Error:', error));
+        .then(response => response.json())
+        .then(data => dibujarBanner(data))
+        .then(data => llamarApiVideos(id, idioma.espanol))
+        /* .catch(error => console.error('Error:', error)); */
 }
 
 function dibujarBanner(json) {
     let bannerHTML = crearBanner(json);
     document.querySelector('.bannerMovie').innerHTML = bannerHTML;
-    document.querySelector('.banner-background').style.background = 
+    document.querySelector('.banner-background').style.background =
     `linear-gradient(0deg ,  #181419ff 1%,#18141900 90%),
     url(https://image.tmdb.org/t/p/original/${json.backdrop_path})`;
 }
@@ -56,5 +60,27 @@ function crearBanner(movie) {
     </div>
     `;
 }
-      
+function crearTrailerMovie(key) {
+    document.querySelector('.video-container').innerHTML = `
+    <iframe src="https://www.youtube.com/embed/${key}" frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen></iframe>
+
+    `
+}
+
+function llamarApiVideos(id, idiomaMovie) {
+
+    fetch(`${API_URL}/movie/${id}/videos?language=${idiomaMovie}`,{
+        headers: {
+            Authorization: `Bearer ${API_KEY}`,
+        },
+    })
+    .then(response => {return response.json()})
+    .then(data => data.results.find(video => video.type == 'Trailer'))
+    .then(video => {console.log(video==null); return video})
+    //si encuentra el trailer en españo o no lo encuentra, busca en ingles
+    .then(video => {video!=null?crearTrailerMovie(video.key):llamarApiVideos(id,idioma.ingles)})   
+
+}
 llamarApi(sessionStorage.getItem('idMovie'))
